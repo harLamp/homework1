8a06d5b9990016b4bcb3a217558aae70d723168a
Vladimir Kharlampidi <harlamp_83@yahoo.com>
2022-09-01 20:51:47 +0300

    написал программу меньше больше

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Определяет какое число меньшее и большее
+Console.WriteLine("Введите число 1");
+
+string frstnum = Console.ReadLine (); // ввод
+
+int a = int.Parse (frstnum); // число
+
+Console.WriteLine ("Введите число 2");
+
+string scndnum = Console.ReadLine (); // ввод
+
+int b = int.Parse (scndnum); // число
+
+if(a < b){
+    Console.Write($"min{a} max{b}");
+}
+else if (a > b){
+    Console.Write($"min{b} max{a}");
+}
