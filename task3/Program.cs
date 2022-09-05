2680eeeba77a9b1a573bb324c9f8be9b6f2a433c
Vladimir Kharlampidi <harlamp_83@yahoo.com>
2022-09-05 16:06:27 +0300

    определяет четное или нечетное

---
+++ task3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Определяет четное или нечетное.
+Console.WriteLine("Введите число ");
+
+string num = Console.ReadLine (); // ввод
+
+int n = int.Parse (num); // число
+
+if (n == 0) {
+
+    Console.Write($"Ввели число {n}, введите другое число");
+
+}
+else {
+    if (n % 2 ==0) {
+
+        Console.Write($"Число {n} четное");
+    }
+
+    else {
+        Console.Write($"Число {n} нечетное");
+    }
+}
