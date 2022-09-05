497042388880d403ecc3bbae70b0c3409b22f6c0
Vladimir Kharlampidi <harlamp_83@yahoo.com>
2022-09-05 16:47:56 +0300

    показывает все четные числа до n.

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показывает все четные числа до n.
+Console.WriteLine("Введите число");
+
+string num = Console.ReadLine (); // ввод
+
+int n = int.Parse (num); // число
+int i = 0;
+if (n < i) 
+{
+        Console.WriteLine("Введите другое число");
+}
+else
+{
+    for (i = 0; i <= n; i += 2)
+    {
+        Console.Write($" {i}");
+    }
+}
